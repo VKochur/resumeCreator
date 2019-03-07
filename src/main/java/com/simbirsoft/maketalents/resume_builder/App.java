9a7befe8a0e9c44302c29a180e42aedebafcc75b
kvv <v.kochurov@rambler.ru>
2019-03-07 13:47:22 +0400

    added maven archetype

---
+++ src/main/java/com/simbirsoft/maketalents/resume_builder/App.java
@@ -0,0 +1,13 @@
+package com.simbirsoft.maketalents.resume_builder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
