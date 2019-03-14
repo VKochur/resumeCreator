8893c66a952802c8f641f1da547e518b4950d68b
kvv <v.kochurov@rambler.ru>
2019-03-14 16:31:20 +0400

    added unit tests

---
+++ src/test/java/com/simbirsoft/maketalents/resume_builder/model/image/impl/html/HtmlResumePrinterTest.java
@@ -0,0 +1,79 @@
+package com.simbirsoft.maketalents.resume_builder.model.image.impl.html;
+
+import com.simbirsoft.maketalents.resume_builder.dao.impl.properties_file_loader.ResumeDaoImplTest;
+import com.simbirsoft.maketalents.resume_builder.entity.Resume;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import java.io.*;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
+public class HtmlResumePrinterTest {
+
+    private String definePathTestClasses() throws URISyntaxException {
+        return (new File(ResumeDaoImplTest.class.getProtectionDomain().getCodeSource().getLocation().toURI())).getAbsolutePath();
+    }
+
+    @Test
+    public void testPrint() throws IOException, URISyntaxException {
+        Resume resume = new Resume();
+        String name = "fio";
+        String dateOfBorn = "dob";
+        resume.setName(name);
+        resume.setDateOfBorn(dateOfBorn);
+
+        HtmlResumePrinter resumePrinter = new HtmlResumePrinter();
+        resumePrinter.setHtmlResumeCodeCreator(new HtmlResumeCodeCreator() {
+            @Override
+            public String getHtmlCode() {
+                return getResume().getName() + getResume().getDateOfBorn();
+            }
+        });
+
+        String fileName = "testHtmlResumePrinter";
+        File workDir = new File(definePathTestClasses() + "\\work");
+        File html = new File(workDir.getAbsolutePath()+"\\" + fileName + ".html");
+        if (workDir.exists()){
+            throw new IllegalStateException("Cannot run test. Dir already exists: " + workDir);
+        } else {
+            try {
+                workDir.mkdir();
+                resumePrinter.setPathDirToFile(workDir.getAbsolutePath());
+                resumePrinter.setNameFile(fileName);
+                resumePrinter.print(resume);
+                checkContentFile(html, "fiodob");
+            } finally {
+                Files.deleteIfExists(html.toPath());
+                Files.delete(workDir.toPath());
+            }
+        }
+    }
+
+    private void checkContentFile(File html, String expectedContent) throws IOException {
+        StringBuilder actualContent = new StringBuilder();
+        String line;
+        try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(html), StandardCharsets.UTF_8.name()))){
+            while ((line = bufferedReader.readLine()) != null) {
+               actualContent.append(line);
+            }
+         assertEquals(expectedContent, actualContent.toString());
+        }
+    }
+
+    @Test(expected = IOException.class)
+    public void testPrint2() throws URISyntaxException, IOException {
+        HtmlResumePrinter resumePrinter = new HtmlResumePrinter();
+        resumePrinter.setHtmlResumeCodeCreator(new HtmlResumeCodeCreator() {
+            @Override
+            public String getHtmlCode() {
+                return null;
+            }
+        });
+        resumePrinter.setNameFile("test");
+        resumePrinter.setPathDirToFile(definePathTestClasses() + "\\work");
+        resumePrinter.print(new Resume());
+    }
+}
---
+++ src/test/java/com/simbirsoft/maketalents/resume_builder/model/image/impl/html/ReplacerHtmlCodeCreatorTest.java
@@ -0,0 +1,31 @@
+package com.simbirsoft.maketalents.resume_builder.model.image.impl.html;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class ReplacerHtmlCodeCreatorTest {
+
+    @Test
+    public void testGetHtmlCode() {
+        ReplacerHtmlCodeCreator codeCreatorCreator = new ReplacerHtmlCodeCreator() {
+            @Override
+            public String getPreCode() {
+                return "${var1} other ${var1} ${var2} ${var3}";
+            }
+
+            @Override
+            public Map<String, String> getSubstitution() {
+                Map<String, String> substitution = new HashMap<>();
+                substitution.put("var1", "VALUE1");
+                substitution.put("var3", "VALUE3");
+                return substitution;
+            }
+        };
+
+        assertEquals("VALUE1 other VALUE1 ${var2} VALUE3", codeCreatorCreator.getHtmlCode());
+    }
+}
