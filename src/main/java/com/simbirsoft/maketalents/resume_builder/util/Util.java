c0ae4ab985cadfed14357b39d139a950ffc1ad2b
kvv <v.kochurov@rambler.ru>
2019-03-31 01:39:00 +0300

    fix util. solve executable path of jar, even if classes not top level in jar

--- src/main/java/com/simbirsoft/maketalents/resume_builder/util/Util.java
+++ src/main/java/com/simbirsoft/maketalents/resume_builder/util/Util.java
@@ -3,18 +3,34 @@ package com.simbirsoft.maketalents.resume_builder.util;
 import com.simbirsoft.maketalents.resume_builder.Main;
 
 import java.io.File;
+import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 
 public class Util {
 
-    //get directory of executable file
+    //get directory of executable file (.class or .jar)
     public static String getPathExecutableDir() {
-        File file = null;
         try {
-            file = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI());
+            URL url = Main.class.getProtectionDomain().getCodeSource().getLocation();
+            switch (url.getProtocol()) {
+                case "file":
+                    File file = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI());
+                    return file.getParent();
+                case "jar":
+                    return solveDirectoryToJar(url.getPath());
+                default:
+                    throw new IllegalStateException("program is not 'class' file and not 'jar', not able solve executable path");
+            }
         } catch (URISyntaxException e) {
-            e.printStackTrace();
+            throw new IllegalStateException(e);
         }
-        return file.getParent();
+    }
+
+
+    private static String solveDirectoryToJar(String path) throws URISyntaxException {
+        String cleanFromExclamationMark = path.substring(0, path.indexOf(".jar!"));
+        URI uri = new URI(cleanFromExclamationMark);
+        return new File(uri).getParent();
     }
 }
