e754a291a7d4285b4b18783cf520d8a104aa8368
kvv <v.kochurov@rambler.ru>
2019-04-15 16:20:21 +0400

    edited view for CRUD resume in db

--- src/main/java/com/simbirsoft/maketalents/resume_builder/launcher/impl/LauncherWeb.java
+++ src/main/java/com/simbirsoft/maketalents/resume_builder/launcher/impl/LauncherWeb.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -29,4 +30,6 @@ public class LauncherWeb implements Launcher {
     public void launch(String[] args) {
         SpringApplication.run(LauncherWeb.class, args);
     }
+
+
 }
